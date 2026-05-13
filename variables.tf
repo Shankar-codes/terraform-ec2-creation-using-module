532a8f6de8d1bd942a60d7448999b47853f948c7
Shankar-codes <harikainteriors@gmail.com>
2026-05-13 12:06:44 +0530

    creating ec2 module using the terraform module

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "ami_id" {
+  type        = string
+  default     = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "sg_ids" {
+  type        = string
+  default     = "sg-020b146d0d1696186"
+  description = "description"
+}
+
+variable "tags" {
+  type        = map()
+  default     = {
+    Name = "catalogue"
+  }
+}
